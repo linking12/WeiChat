1a3f29bbdd930620b463f4d2da0a879de33c3180
linking12 <shimingliu.copyboy@gmail.com>
2013-09-24 17:08:40 +0800

    spring mvc & login
    by chenbo

---
+++ src/main/java/net/chat/Constants/PageConstants.java
@@ -0,0 +1,14 @@
+package net.chat.Constants;
+
+public final class PageConstants {
+
+	/**
+	 * 登录页面
+	 */
+	public static final String PAGE_LOGIN="login";
+	
+	/**
+	 * 登录页面后
+	 */
+	public static final String PAGE_LOGIN_1="index";
+}
