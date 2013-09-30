1542f26e88b6379e47680f25c9ffcbd83efee228
linking12 <apple@192.168.1.102>
2013-09-30 18:50:47 +0800

    modify account Service

--- src/main/java/net/chat/service/AccountService.java
+++ src/main/java/net/chat/service/AccountService.java
@@ -7,58 +7,15 @@ import org.springframework.data.domain.Page;
 
 public interface AccountService {
 
-	/**
-	 * 保存公共账户信息
-	 * 
-	 * @param account
-	 */
 	public void saveAccount(WxAccount account);
-
-	/**
-	 * 编辑公共账户信息
-	 * 
-	 * @param account
-	 */
 
 	public void editAccount(WxAccount account);
 
-	/**
-	 * 删除公共账户，也可停用公共账户
-	 * 
-	 * @TODO
-	 * 
-	 * @param accountId
-	 */
-
 	public void deleteAccount(Long accountId);
-
-	/**
-	 * 列出所有公共账户
-	 * 
-	 * @param pageNo
-	 * @param pageSize
-	 * @return
-	 */
 
 	public Page<WxAccount> listAllAcount(int pageNo, int pageSize);
 
-	/**
-	 * 根据公共账户ID获取公共账户信息
-	 * 
-	 * @param accountId
-	 * @return
-	 */
-
 	public WxAccount findAcountById(Long accountId);
-
-	/**
-	 * 查询公共账户的的信息
-	 * 
-	 * @param accountId
-	 * @param pageNo
-	 * @param pageSize
-	 * @return
-	 */
 
 	public Page<WxMsgType> queryAllMessageTypeInAccount(Long accountId,
 			int pageNo, int pageSize);
