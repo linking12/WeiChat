cfdd2df5b68b21bbf238534211a1abcd3953acec
linking12 <apple@192.168.1.100>
2013-10-24 00:25:11 +0800

    修改整个缓存存活时间

--- src/main/java/net/chat/integration/vo/CacheContant.java
+++ src/main/java/net/chat/integration/vo/CacheContant.java
@@ -5,17 +5,17 @@ import net.chat.utils.Cache;
 public class CacheContant {
 
 	public final static Cache<String, Object> sourceCache = new Cache<String, Object>(
-			100, 100, 10);
+			5 * 60 * 1000, 100, 10);
 
 	public final static Cache<String, Object> accountCache = new Cache<String, Object>(
-			100, 100, 10);
+			5 * 60 * 1000, 100, 10);
 	public final static Cache<String, Object> gameCache = new Cache<String, Object>(
-			100, 100, 10);
+			5 * 60 * 1000, 100, 10);
 
 	public final static Cache<String, Object> urlseqsCache = new Cache<String, Object>(
-			100, 100, 10);
+			5 * 60 * 1000, 100, 10);
 
 	public final static Cache<String, Object> autoReplayAndCmdCache = new Cache<String, Object>(
-			100, 100, 10);
+			5 * 60 * 1000, 100, 10);
 
 }
