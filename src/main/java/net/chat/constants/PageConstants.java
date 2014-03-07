e3b59213519025519636139f5901457409631ace
linking12 <think@bo>
2014-03-07 14:40:45 +0800

    change

--- src/main/java/net/chat/constants/PageConstants.java
+++ src/main/java/net/chat/constants/PageConstants.java
@@ -294,8 +294,8 @@ public final class PageConstants {
 	 */
 	public static List<SimpleBean> buildEventTypesList() {
 		List<SimpleBean> lst = new ArrayList<SimpleBean>(1);
-		lst.add(new SimpleBean("message", "回复消息"));
 		lst.add(new SimpleBean("url", "超链接"));
+		lst.add(new SimpleBean("message", "回复消息"));
 		return lst;
 	}
 
